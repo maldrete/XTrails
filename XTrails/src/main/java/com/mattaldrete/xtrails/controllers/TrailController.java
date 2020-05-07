485b14e51f0532e775f593d90063d80e962af9a1
Matt <mra286@humboldt.edu>
2020-05-07 17:28:23 -0600

    delete trail works

--- XTrails/src/main/java/com/mattaldrete/xtrails/controllers/TrailController.java
+++ XTrails/src/main/java/com/mattaldrete/xtrails/controllers/TrailController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -65,6 +66,14 @@ public class TrailController {
 	public Trail updateTrail(@RequestBody Trail updatedTrail) {
 		Trail trailWithUpdates = trailSvc.updateTrail(updatedTrail);
 		return trailWithUpdates;
+	}
+	
+	@DeleteMapping("trails/{trailName}")
+	public Boolean deleteTrail(@PathVariable String trailName) {
+//		maybe only admin function
+		Boolean deleted = false;
+		trailSvc.deleteTrail(trailName);
+		return true;
 	}
 
 }
