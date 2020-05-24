9504af226d4d6c2cda9b74f8dc515658e25da015
Matt <mra286@humboldt.edu>
2020-05-23 20:20:20 -0600

    created user service, get user method complete

---
+++ ngXTrails/src/app/services/user.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AuthService } from './auth.service';
+import { Observable, throwError } from 'rxjs';
+import { User } from '../entities/user/user';
+import { catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private baseURL = environment.baseUrl + 'api/me';
+
+  constructor(private http: HttpClient, private authSvc: AuthService) { }
+
+  // M E T H O D S
+  getUser(): Observable<User> {
+    const credentials = this.authSvc.getCredentials();
+
+    let httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': `Basic ${credentials}`
+      })
+    };
+
+    return this.http.get<User>(this.baseURL, httpOptions).pipe(
+      catchError((err: any) => {
+        console.log('user.service.getUser(): Error retrieving user');
+        console.log(err);
+        return throwError(err);
+      })
+    )
+
+
+  }
+
+
+
+
+}
